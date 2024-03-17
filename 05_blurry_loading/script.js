9f7eed5cd4a1468787521921336fbd56c8d25b53
Sidney Shafer <sidney@mnshafers.com>
2024-03-16 19:06:51 -0500

    Update js code

--- 05_blurry_loading/script.js
+++ 05_blurry_loading/script.js
@@ -11,15 +11,12 @@ function blurring() {
       clearInterval(int)
    }
 
-   // text blur
    loadText.innerText = `${load}%`
    loadText.style.opacity = scale(load, 0, 100, 1, 0)
 
-   // image blur
    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-// Handy Function ---stack overflow
 const scale = (num, in_min, in_max, out_min, out_max) => {
    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
