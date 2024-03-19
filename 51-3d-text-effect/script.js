2a8f67eed7ae676f667533c0390fd9e073a9051c
Sidney Shafer <sidney@mnshafers.com>
2024-03-19 14:21:11 -0500

    Delete project

--- 51-3d-text-effect/script.js
+++
@@ -1,8 +0,0 @@
-var text = document.getElementById('text')
-var shadow = '';
-
-for(var i = 0; i < 25; i++) {
-    shadow +=(shadow? ',':'')+ -i * 1 +'px ' + i*1 + 'px 0 #d9d9d9';
-}
-
-text.style.textShadow = shadow
